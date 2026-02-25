e44d2fec395f03669b46ac3b073ea302a013a8ff
Laura León <lauraleon@MacBook-Air-de-Laura.local>
2026-02-25 08:56:14 -0500

    Añade test de creacion de bot gpt

--- test_crear_bot_gpt.py
+++ test_crear_bot_gpt.py
@@ -5,7 +5,7 @@ usuario = "auto-bot-6@pantera.com"
 contraseña = "auto-bot-6@pantera.com"
 
 def test_crear_bot(page: Page):
-    page.set_viewport_size({"width": 1920, "height": 1080})
+    page.set_viewport_size({"width": 1440, "height": 900})
     page.set_default_timeout(30000)
 
     page.goto(url)
@@ -24,15 +24,18 @@ def test_crear_bot(page: Page):
 
     #Crear bot
     page.get_by_role("button", name="Crear Bot").click()
-    page.get_by_role("textbox", name="Escriba el nombre del bot aquí").fill("QA Test Laura")
+    page.get_by_role("textbox", name="Escriba el nombre del bot aquí").fill("QA Test Laura Bot GPT")
     page.get_by_role("button", name="Seleccionar").click()
-    page.get_by_role("button", name="General").click()
-    page.get_by_role("button", name="Siguiente").click()
-    page.get_by_role("button", name="Iniciar").click()
+    page.get_by_role("button", name="ChattiGPT").click()
+    page.get_by_role("textbox", name="Ejemplo: Usted es un experto [rol o profesión que asumirá GPT]. Crea [tema o tarea]. El objetivo es [contexto]. El contenido es para [a quién dirige la respuesta]. Tus directrices para escribir son [restricciones]").fill("Eres un chatbot especializado en atención al cliente para Chattigo")
+    page.get_by_text("Configuración Avanzada").click()
+    page.get_by_role("button", name="Seleccionar").click()
+    page.get_by_role("button", name="gpt-4o", exact=True).click()
+    page.get_by_role("button", name="Comenzar").click()
 
     #Cerrar Pop Ups del editor de bot
-    page.locator("chattigo-bots-configuration-component .modal-bot-optimization__action").click()
-    page.locator("app-modal-capi").first.get_by_role("button", name="Entendido").click()
+    page.locator("app-gpt-services").get_by_role("button", name="Entendido").click()
+    page.locator("app-modal-gpt").get_by_role("button", name="Entendido").click()
     
     expect(page.get_by_role("button", name="Iniciar edición")).to_be_visible()
     
