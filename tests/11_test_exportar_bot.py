d7627643a1b14390509ef88d4c36738e8bf6017d
Laura León <lauraleon@192.168.1.5>
2026-03-03 08:39:23 -0500

    corrige auto exportar bot

--- tests/11_test_exportar_bot.py
+++ tests/11_test_exportar_bot.py
@@ -1,20 +1,10 @@
-import os
-import pytest
 from playwright.sync_api import Page, expect
 
-def test_exportar_bot_chattigo(page: Page):
-    bot_card = page.locator("div.card-bot").filter(has_text="laura")
+def test_exportar_bot(page: Page):
+    page.get_by_role("button").nth(2).click()
 
-    bot_card.get_by_role("button").click()
-
-    with page.expect_download() as download_info:
+    
+    with page.expect_download():
         page.get_by_role("paragraph").filter(has_text="Exportar proyecto").click()
-    
-    download = download_info.value
-    
-    path = f"./downloads/{download.suggested_filename}"
-    download.save_as(path)
-    
-    assert os.path.exists(path), "El archivo no se descargó correctamente"
     
     expect(page.get_by_text("El proyecto se ha exportado")).to_be_visible()
